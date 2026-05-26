c558824a02dfd81ee4ee136a37c2fd952df5d171
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-05-26 11:36:48 +0900

    Route instructor dashboard by query param

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { InstructorDashboard } from './components/InstructorDashboard';
 import { LearnerShell } from './components/LearnerShell';
 
 function App() {
+  const params = new URLSearchParams(window.location.search);
+  const view = params.get('view');
+
+  if (view === 'instructor') {
+    return <InstructorDashboard />;
+  }
+
   return <LearnerShell />;
 }
 
