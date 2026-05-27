86ad648b817690e41e353e54fb56db5e4a340f34
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-05-28 08:45:20 +0900

    Apply pressure situation overrides as final layer

--- src/data/roundOverridePipeline.ts
+++ src/data/roundOverridePipeline.ts
@@ -1,6 +1,7 @@
 import type { Round } from '../types';
 import { fieldLanguageOverrides } from './fieldLanguageOverrides';
 import { finalRoundOverrides } from './finalRoundOverrides';
+import { pressureSituationOverrides } from './pressureSituationOverrides';
 import { r01RefinementOverrides } from './r01RefinementOverrides';
 import { roundOverrides } from './roundOverrides';
 
@@ -9,6 +10,7 @@ const overrideLayers: Array<Partial<Record<Round['id'], Partial<Round>>>> = [
   finalRoundOverrides,
   r01RefinementOverrides,
   fieldLanguageOverrides,
+  pressureSituationOverrides,
 ];
 
 export function applyRoundOverrides(round: Round): Round {
