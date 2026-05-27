1e81a9c63aaccdf21ac9fa6c5197e67b112cc9a0
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-05-28 06:50:43 +0900

    Sort normalized rounds by finalized session order

--- src/data/normalizedRounds.ts
+++ src/data/normalizedRounds.ts
@@ -2,5 +2,10 @@ import type { Round } from '../types';
 import { normalizeObject } from './roundNormalization';
 import { applyRoundOverrides } from './roundOverridePipeline';
 import { rounds as sourceRounds } from './rounds';
+import { sessions } from './sessions';
 
-export const rounds: Round[] = sourceRounds.map((round) => normalizeObject(applyRoundOverrides(round)));
+const sessionRoundOrder = sessions.flatMap((session) => session.roundIds);
+
+export const rounds: Round[] = sourceRounds
+  .map((round) => normalizeObject(applyRoundOverrides(round)))
+  .sort((a, b) => sessionRoundOrder.indexOf(a.id) - sessionRoundOrder.indexOf(b.id));
