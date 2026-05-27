5d504e639ca3d2140f2829be8e4e57d0dad0e0e1
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-05-28 07:13:14 +0900

    Use shared round order selector in normalized rounds

--- src/data/normalizedRounds.ts
+++ src/data/normalizedRounds.ts
@@ -1,11 +1,10 @@
 import type { Round } from '../types';
+import { getRoundOrderIndex } from '../lib/roundSelectors';
 import { normalizeObject } from './roundNormalization';
 import { applyRoundOverrides } from './roundOverridePipeline';
 import { rounds as sourceRounds } from './rounds';
 import { sessions } from './sessions';
 
-const sessionRoundOrder = sessions.flatMap((session) => session.roundIds);
-
 export const rounds: Round[] = sourceRounds
   .map((round) => normalizeObject(applyRoundOverrides(round)))
-  .sort((a, b) => sessionRoundOrder.indexOf(a.id) - sessionRoundOrder.indexOf(b.id));
+  .sort((a, b) => getRoundOrderIndex(a.id, sessions) - getRoundOrderIndex(b.id, sessions));
