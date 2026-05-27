74748b5c676eeec0e0a39bf259121de9d7746333
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-05-27 10:12:04 +0900

    Safely normalize leader name

--- src/data/normalizedRounds.ts
+++ src/data/normalizedRounds.ts
@@ -11,12 +11,29 @@ const replacements: Array<[string, string]> = [
   ['주임', '대리'],
 ];
 
+const LEADER_TOKEN = '__KAC_LEADER__';
+const MANAGER_LEVEL_TOKEN = '__KAC_MANAGER_LEVEL__';
+const TEST_MANAGER_TOKEN = '__KAC_TEST_MANAGER__';
+
+function normalizeLeaderName(value: string) {
+  const protectedText = value
+    .replaceAll('김원중 과장', LEADER_TOKEN)
+    .replaceAll('과장급', MANAGER_LEVEL_TOKEN)
+    .replaceAll('테스트과장', TEST_MANAGER_TOKEN)
+    .replaceAll('나는 한국공항공사 과장급 중간관리자입니다', `나는 한국공항공사 ${LEADER_TOKEN}입니다`)
+    .replaceAll('나는 한국공항공사 김원중 과장입니다', `나는 한국공항공사 ${LEADER_TOKEN}입니다`);
+
+  return protectedText
+    .replaceAll('과장', LEADER_TOKEN)
+    .replaceAll(LEADER_TOKEN, '김원중 과장')
+    .replaceAll(MANAGER_LEVEL_TOKEN, '과장급')
+    .replaceAll(TEST_MANAGER_TOKEN, '테스트과장');
+}
+
 function normalizeText(value: string) {
   const normalizedNames = replacements.reduce((current, [from, to]) => current.split(from).join(to), value);
 
-  return normalizedNames
-    .replaceAll('나는 한국공항공사 과장급 중간관리자입니다', '나는 한국공항공사 김원중 과장입니다')
-    .replace(/과장(?!급)/g, '김원중 과장');
+  return normalizeLeaderName(normalizedNames);
 }
 
 function normalizeObject<T>(value: T): T {
