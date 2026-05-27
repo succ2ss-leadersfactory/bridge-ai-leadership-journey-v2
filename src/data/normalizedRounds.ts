f6188047b374ab9cfdf43c3b7a48e07ada523a22
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-05-27 10:32:57 +0900

    Name leader Kim Wonjung
    Name leader Kim Wonjung.

--- src/data/normalizedRounds.ts
+++ src/data/normalizedRounds.ts
@@ -11,8 +11,35 @@ const replacements: Array<[string, string]> = [
   ['주임', '대리'],
 ];
 
+const LEADER_TOKEN = '__KAC_LEADER__';
+const MANAGER_LEVEL_TOKEN = '__KAC_MANAGER_LEVEL__';
+const TEST_MANAGER_TOKEN = '__KAC_TEST_MANAGER__';
+
+function replaceEvery(value: string, from: string, to: string) {
+  return value.split(from).join(to);
+}
+
+function normalizeLeaderName(value: string) {
+  const protectedText = [
+    ['김원중 과장', LEADER_TOKEN],
+    ['과장급', MANAGER_LEVEL_TOKEN],
+    ['테스트과장', TEST_MANAGER_TOKEN],
+    ['나는 한국공항공사 과장급 중간관리자입니다', `나는 한국공항공사 ${LEADER_TOKEN}입니다`],
+    ['나는 한국공항공사 김원중 과장입니다', `나는 한국공항공사 ${LEADER_TOKEN}입니다`],
+  ].reduce((current, [from, to]) => replaceEvery(current, from, to), value);
+
+  return [
+    ['과장', LEADER_TOKEN],
+    [LEADER_TOKEN, '김원중 과장'],
+    [MANAGER_LEVEL_TOKEN, '과장급'],
+    [TEST_MANAGER_TOKEN, '테스트과장'],
+  ].reduce((current, [from, to]) => replaceEvery(current, from, to), protectedText);
+}
+
 function normalizeText(value: string) {
-  return replacements.reduce((current, [from, to]) => current.split(from).join(to), value);
+  const normalizedNames = replacements.reduce((current, [from, to]) => current.split(from).join(to), value);
+
+  return normalizeLeaderName(normalizedNames);
 }
 
 function normalizeObject<T>(value: T): T {
