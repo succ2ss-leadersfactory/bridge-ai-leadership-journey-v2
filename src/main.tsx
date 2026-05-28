f76b93da87a3933cfb5a8c09e6a4bb162217702f
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-05-28 13:37:54 +0900

    Load KAC UI override styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './completedRounds.css';
 import './developmentCards.css';
+import './kacUiOverrides.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
