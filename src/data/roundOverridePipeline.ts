5e3ac69496055f34610a257a61ba31e07438f1ca
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-05-28 10:00:19 +0900

    Apply balanced first dilemma choice overrides

--- src/data/roundOverridePipeline.ts
+++ src/data/roundOverridePipeline.ts
@@ -1,6 +1,7 @@
 import type { Round } from '../types';
 import { fieldLanguageOverrides } from './fieldLanguageOverrides';
 import { finalRoundOverrides } from './finalRoundOverrides';
+import { firstDilemmaChoiceOverrides } from './firstDilemmaChoiceOverrides';
 import { pressureSituationOverrides } from './pressureSituationOverrides';
 import { r01RefinementOverrides } from './r01RefinementOverrides';
 import { roundOverrides } from './roundOverrides';
@@ -11,6 +12,7 @@ const overrideLayers: Array<Partial<Record<Round['id'], Partial<Round>>>> = [
   r01RefinementOverrides,
   fieldLanguageOverrides,
   pressureSituationOverrides,
+  firstDilemmaChoiceOverrides,
 ];
 
 export function applyRoundOverrides(round: Round): Round {
