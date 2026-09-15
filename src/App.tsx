acb5a0a7128e1d7992b287272a41763ffdd7c156
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-09-16 08:46:50 +0900

    Switch app to learner-only local mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import { InstructorDashboard } from './components/InstructorDashboard';
 import { LearnerShell } from './components/LearnerShell';
-import { PreflightCheck } from './components/PreflightCheck';
 
 function App() {
-  const params = new URLSearchParams(window.location.search);
-  const view = params.get('view');
-
-  if (view === 'instructor') {
-    return <InstructorDashboard />;
-  }
-
-  if (view === 'check') {
-    return <PreflightCheck />;
-  }
-
   return <LearnerShell />;
 }
 
