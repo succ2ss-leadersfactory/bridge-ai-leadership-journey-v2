db7992ac06e961699e8b6b52710458ba350cb3a2
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-09-16 11:15:52 +0900

    Switch to action-changing simulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { V3LearnerShellRedTeam } from './components/V3LearnerShellRedTeam';
+import { V4ActionSimulationShell } from './components/V4ActionSimulationShell';
 import { v3Cases } from './v3/cases';
 import { applyGenericCaseOverrides } from './v3/genericCaseOverrides';
 import { applyHonorificTerminology } from './v3/honorificTerminology';
 import { applyTheoryTerminology } from './v3/theoryTerminology';
+import { applyActionSimulationOverrides } from './v4/actionSimulationConfig';
 
 applyGenericCaseOverrides(v3Cases);
 applyTheoryTerminology(v3Cases);
 applyHonorificTerminology(v3Cases);
+applyActionSimulationOverrides(v3Cases);
 
 function App() {
-  return <V3LearnerShellRedTeam />;
+  return <V4ActionSimulationShell />;
 }
 
 export default App;
