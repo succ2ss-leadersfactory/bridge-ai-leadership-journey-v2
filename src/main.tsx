8108b7554922f67159eb4b0e0b4d029c469a1215
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-09-16 12:48:17 +0900

    Apply shared plain-language UI polish in mobile and PC modes

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './v4/v4Action.css';
 import './v4/v4InstructorPc.css';
 import './v4/v4InstructorDiscussionReveal.css';
 import './v4/instructorDiscussionReveal';
+import './v4/commonUiPolish';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
