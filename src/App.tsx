6eea52d8eb456c4a2d3cf6f01c53db7e0c10a81a
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-09-16 09:12:31 +0900

    Switch app entry to v3 A-F learner experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { LearnerShell } from './components/LearnerShell';
+import { V3LearnerShell } from './components/V3LearnerShell';
 
 function App() {
-  return <LearnerShell />;
+  return <V3LearnerShell />;
 }
 
 export default App;
