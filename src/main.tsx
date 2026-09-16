a52501cf156e653619094dd621fd00f883b015e0
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-09-17 08:28:49 +0900

    Enable iPhone Safari optimizations in app bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { installIPhoneSafariOptimizations } from './lib/iphoneSafari';
 import './styles.css';
 import './completedRounds.css';
 import './developmentCards.css';
 import './kacUiOverrides.css';
 import './readabilityPolish.css';
+import './iphoneSafari.css';
+
+installIPhoneSafariOptimizations();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
