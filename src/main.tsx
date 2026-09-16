b602527d5cb9ba01691091a6b9a5cf584b101038
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-09-16 11:15:57 +0900

    Load action simulation styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './v3/v3.css';
 import './v3/v3RedTeam.css';
+import './v4/v4Action.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
