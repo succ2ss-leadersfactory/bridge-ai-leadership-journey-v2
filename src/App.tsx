4ec7d2731974dbfd3cf4a6f02dac9f00bf2e9beb
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-09-16 09:59:27 +0900

    Apply full-name plus title honorific rule

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { V3LearnerShellRedTeam } from './components/V3LearnerShellRedTeam';
 import { v3Cases } from './v3/cases';
 import { applyGenericCaseOverrides } from './v3/genericCaseOverrides';
+import { applyHonorificTerminology } from './v3/honorificTerminology';
 import { applyTheoryTerminology } from './v3/theoryTerminology';
 
 applyGenericCaseOverrides(v3Cases);
 applyTheoryTerminology(v3Cases);
+applyHonorificTerminology(v3Cases);
 
 function App() {
   return <V3LearnerShellRedTeam />;
