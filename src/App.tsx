8243efb5f736648caccff8a5cb2852d7ed865cf3
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-09-16 09:32:37 +0900

    Apply general workplace case master in v3.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { V3LearnerShellRedTeam } from './components/V3LearnerShellRedTeam';
+import { v3Cases } from './v3/cases';
+import { applyGenericCaseOverrides } from './v3/genericCaseOverrides';
+
+applyGenericCaseOverrides(v3Cases);
 
 function App() {
   return <V3LearnerShellRedTeam />;
