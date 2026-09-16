139ece0df47e2a6af868843034a0c8eeb2695acb
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-09-16 12:25:12 +0900

    Restore v4.1 PC layout and add discussion reveal

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import './v3/v3.css';
 import './v3/v3RedTeam.css';
 import './v4/v4Action.css';
 import './v4/v4InstructorPc.css';
-import './v4/v4Instructor169.css';
+import './v4/v4InstructorDiscussionReveal.css';
+import './v4/instructorDiscussionReveal';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
